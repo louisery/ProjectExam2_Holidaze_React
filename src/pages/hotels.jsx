a803169668f6462b16ed33bcf0f35fe7c380d123
Louise Ryste <louiseryste@gmail.com>
2019-05-28 00:48:16 +0200

    mirrored images and text on hotels page

--- src/pages/hotels.jsx
+++ src/pages/hotels.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import axios from "axios";
 import HotelComponent from "../components/hotelComponent";
 
+import { Link } from "react-router-dom";
+
 export default class Hotel extends React.Component {
   state = {
     hotels: []
@@ -24,13 +26,8 @@ export default class Hotel extends React.Component {
           <h1>The Hotels</h1>
         </div>
         <div className="[ col-lg-12 col-md-12 col-sm-12 col-xs-12 ]">
-          {this.state.hotels.map(hotels => (
-            <Hotels key={hotels.id} {...hotels} />
-          ))}
-        </div>
-        <div className="[ col-lg-12 col-md-12 col-sm-12 col-xs-12 ]">
-          {this.state.hotels.map(hotels => (
-            <HotelComponent />
+          {this.state.hotels.map((hotels, idx) => (
+            <Hotels key={hotels.id} mirrored={idx % 2 === 0} {...hotels} />
           ))}
         </div>
       </div>
@@ -39,24 +36,43 @@ export default class Hotel extends React.Component {
 }
 
 const Hotels = props => {
-  return (
-    <div className="[ row ] [ split-1 ] [ hotels ]">
-      <div className="[ col-sm-6 ] [ text-center ]">
-        <img
-          src={props.imageUrl}
-          className="[ img-fluid ] [ img--size ] [ img--rounded ]"
-        />
-      </div>
-      <div className="[ col-sm-6 ]">
-        <div className="[ hotels--center ]">
-          <h2 className="[ hotels__heading ]">{props.establishmentName}</h2>
-          <hr className="[ hr--heading ]" />
-          <p id="hotels__description">
-            <b>Description:</b> {props.description}
-          </p>
-          <button className="[ btn ]">Book Hotel</button>
-        </div>
+  let { mirrored } = props;
+  let imageHalf = props => (
+    <div className="[ col-sm-6 ] [ text-center ]">
+      <img
+        src={props.imageUrl}
+        className="[ img-fluid ] [ img--size ] [ img--rounded ]"
+      />
+    </div>
+  );
+  let textHalf = props => (
+    <div className="[ col-sm-6 ]">
+      <div className="[ hotels--center ]">
+        <h2 className="[ hotels__heading ]">{props.establishmentName}</h2>
+        <hr className="[ hr--heading ]" />
+        <p id="hotels__description">
+          <b>Description:</b> {props.description}
+        </p>
+        <Link to={`/hotel-specific/${props.id}`} className="btn">
+          Book Hotel
+        </Link>
       </div>
     </div>
   );
+
+  if (mirrored) {
+    return (
+      <div className="[ row ] [ split-1 ] [ hotels ]">
+        {imageHalf(props)}
+        {textHalf(props)}
+      </div>
+    );
+  } else {
+    return (
+      <div className="[ row ] [ split-1 ] [ hotels ]">
+        {textHalf(props)}
+        {imageHalf(props)}
+      </div>
+    );
+  }
 };
